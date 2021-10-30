8d372f1fd916c9a761b47fae9fc492261bb6bbfa
stctai <84058750+stctai@users.noreply.github.com>
2021-10-29 22:43:35 -0700

    Adding part 8, the code of application

---
+++ Spenzoo/db/mySQLiteDB.js
@@ -0,0 +1,133 @@
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
+sqlite3.verbose();
+
+async function connect() {
+  return open({
+    filename: "./db/spenzoo.db",
+    driver: sqlite3.Database,
+  });
+}
+
+async function getUsers() {
+  const db = await connect();
+
+  return await db.all("SELECT * FROM User ORDER BY name LIMIT 20");
+}
+
+async function createUser(newUser) {
+  const db = await connect();
+
+  const stmt = await db.prepare(`INSERT INTO
+    User(userId, name, gender, dateOfBirth, state)
+    VALUES (:userId, :name, :gender, :dateOfBirth, :state)
+  `);
+
+  stmt.bind({
+    ":userId": newUser.userId,
+    ":name": newUser.name,
+    ":gender": newUser.gender,
+    ":dateOfBirth": newUser.dateOfBirth,
+    ":state": newUser.state,
+  });
+
+  return await stmt.run();
+}
+
+async function getUserById(userId) {
+  const db = await connect();
+
+  const stmt = await db.prepare(`SELECT *
+    FROM User
+    WHERE
+      userId = :userId
+  `);
+
+  stmt.bind({
+    ":userId": userId,
+  });
+
+  return await stmt.get();
+}
+
+async function deleteUser(userToDelete) {
+  const db = await connect();
+
+  const stmt = await db.prepare(`DELETE FROM
+    User
+    WHERE userId = :userToDelete
+  `);
+
+  stmt.bind({
+    ":userToDelete": userToDelete.userId,
+  });
+
+  return await stmt.run();
+}
+
+async function getUserBudgets() {
+  const db = await connect();
+
+  return await db.all(
+    "SELECT * FROM User_Budget ORDER BY budgetId DESC LIMIT 20"
+  );
+}
+
+async function createUserBudget(newUserBudget) {
+  const db = await connect();
+
+  const stmt = await db.prepare(`INSERT INTO
+    User_Budget(UserId, budgetId)
+    VALUES (:UserId, :budgetId)
+  `);
+
+  stmt.bind({
+    ":UserId": newUserBudget.UserId,
+    ":budgetId": newUserBudget.budgetId,
+  });
+
+  return await stmt.run();
+}
+
+async function getUserBudgetById(UserId) {
+  const db = await connect();
+
+  const stmt = await db.prepare(`SELECT *
+    FROM User_Budget
+    WHERE
+      UserId = $UserId
+  `);
+
+  stmt.bind({
+    ":UserId": UserId,
+  });
+
+  return await stmt.get();
+}
+
+async function deleteUserBudget(userBudgetToDelete) {
+  console.log("try to delete");
+  const db = await connect();
+  console.log("112");
+  const stmt = await db.prepare(`DELETE FROM
+    User_Budget
+    WHERE UserId = :userBudgetToDelete
+  `);
+
+  stmt.bind({
+    ":deleteUserBudget": userBudgetToDelete.UserId,
+  });
+  console.log("122");
+  return await stmt.run();
+}
+
+module.exports.getUsers = getUsers;
+module.exports.createUser = createUser;
+module.exports.getUserById = getUserById;
+module.exports.deleteUser = deleteUser;
+
+module.exports.getUserBudgets = getUserBudgets;
+module.exports.createUserBudget = createUserBudget;
+module.exports.getUserBudgetById = getUserBudgetById;
+module.exports.deleteUserBudget = deleteUserBudget;
---
+++ Spenzoo/routes/index.js
@@ -0,0 +1,121 @@
+let express = require("express");
+let router = express.Router();
+
+const myDB = require("../db/MySqliteDB.js");
+
+/* GET home page. */
+router.get("/", async function (req, res) {
+  console.log("Got request for /");
+
+  const users = await myDB.getUsers();
+  const user_budget = await myDB.getUserBudgets();
+
+  // console.log("got User", users);
+  // console.log("got User", user_budget);
+
+  // render the _index_ template with the user attrib as the list of user
+  res.render("index", { user: users, user_budget: user_budget });
+  // res.render("index", { user_budget: user_budget });
+});
+
+/* GET user details. */
+router.get("/user/:userId", async function (req, res) {
+  console.log("Got users details");
+
+  const userId = req.params.userId;
+
+  console.log("got user details ", userId);
+
+  const user = await myDB.getUserById(userId);
+
+  console.log("user created");
+
+  res.render("userDetails", { user: user });
+});
+
+/* POST create user. */
+router.post("/user/create", async function (req, res) {
+  console.log("Got post create/user");
+
+  const user = req.body;
+
+  console.log("got create user", user);
+
+  await myDB.createUser(user);
+
+  console.log("user created");
+
+  res.redirect("/");
+});
+
+/* POST create user. */
+router.post("/user/delete", async function (req, res) {
+  console.log("Got post delete user");
+
+  const user = req.body;
+
+  console.log("got delete user", user);
+
+  await myDB.deleteUser(user);
+
+  console.log("user deleted");
+
+  res.redirect("/");
+});
+
+// /* GET home page. */
+// router.get("/", async function (req, res) {
+//   console.log("Got request for /");
+
+//   console.log("got user_budget", user_budget);
+
+//   // render the _index_ template with the user attrib as the list of user
+
+// });
+
+/* GET user details. */
+router.get("/user_budget/:UserId", async function (req, res) {
+  console.log("Got user_budget details");
+
+  const UserId = req.params.UserId;
+
+  console.log("got user details ", UserId);
+
+  const user_budget = await myDB.getUserBudgetById(UserId);
+
+  console.log("user created");
+
+  res.render("userDetails", { user_budget: user_budget });
+});
+
+/* POST create user_budget. */
+router.post("/user_budget/create", async function (req, res) {
+  console.log("Got post create/user_budget");
+
+  const user_budget = req.body;
+
+  console.log("got create user_budget", user_budget);
+
+  await myDB.createUserBudget(user_budget);
+
+  console.log("user_budget created");
+
+  res.redirect("/");
+});
+
+/* POST create user_budget. */
+router.post("/user_budget/delete", async function (req, res) {
+  console.log("Got post delete user_budget");
+
+  const user_budget = req.body;
+
+  console.log("got delete user_budget", user_budget);
+
+  await myDB.deleteUserBudget(user_budget);
+
+  console.log("user_budget deleted");
+
+  res.redirect("/");
+});
+
+module.exports = router;
